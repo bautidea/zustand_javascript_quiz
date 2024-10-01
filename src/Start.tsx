274930590810da355909004f7475ade474fc4e99
bautidea <juanbautistadeangelis@gmail.com>
2024-10-01 08:55:09 -0300

    Creating global state.

--- src/Start.tsx
+++ src/Start.tsx
@@ -1,8 +1,15 @@
 import { Button } from '@mui/material';
+import { useQuestionStore } from './store/question';
 
 export function Start() {
+  const fetchQuestions = useQuestionStore((state) => state.fetchQuestion);
+
+  const handleClick = () => {
+    fetchQuestions(5);
+  };
+
   return (
-    <Button onClick={() => {}} variant="contained">
+    <Button onClick={handleClick} variant="contained">
       Start
     </Button>
   );
