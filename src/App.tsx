0ebcbbc8462ed0e5beea1bb31c5e78401ecbfb14
bautidea <juanbautistadeangelis@gmail.com>
2024-10-02 09:11:38 -0300

    Setting global state.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { Container, Stack, Typography } from '@mui/material';
 import './App.css';
 import { JavaScriptIcon } from './assets/Icons';
 import { Start } from './Start';
+import { useQuestionStore } from './store/question';
+import { type Question } from './types';
 
 function App() {
+  const questions: Question[] = useQuestionStore((state) => state.questions);
+
   return (
     <main>
       <Container maxWidth="sm">
--- src/store/question.ts
+++ src/store/question.ts
@@ -11,16 +11,26 @@ export const useQuestionStore = create<State>((set) => ({
   questions: [],
   currentQuestion: 0,
   fetchQuestion: async (limit) => {
-    set((state) => ({
-      questions: [
-        {
-          'id': 1,
-          'question': '¿Cuál es la salida de este código?',
-          'code': 'console.log(typeof NaN)',
-          'answers': ['undefined', 'NaN', 'string', 'number'],
-          'correctAnswer': 3,
-        },
-      ],
-    }));
+    // Fetching from public data.
+    const response = await fetch('http://localhost:5173/data.json');
+
+    if (!response.ok)
+      throw new Error('Couldnt load quiz questions, try again.');
+
+    const data = await response.json();
+
+    // From 50 question im keeping randomly up to the limit variable.
+    const questions = data
+      .map((q: Question) => ({ q, rand: Math.random() }))
+      .sort(
+        (
+          { rand: aRand }: { q: Question; rand: number },
+          { rand: bRand }: { q: Question; rand: number }
+        ) => aRand - bRand
+      )
+      .map(({ q }: { q: Question; rand: number }) => q)
+      .slice(0, limit);
+
+    set({ questions });
   },
 }));
